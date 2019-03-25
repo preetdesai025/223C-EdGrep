97ea6b8fd6fa28d98e4c3bac060ebcb70a85baca
preetdesai025 <43251600+preetdesai025@users.noreply.github.com>
2019-03-24 21:43:55 -0700

    Add files via upload

---
+++ edGrep.h
@@ -0,0 +1,60 @@
+//edgrep.h
+
+/* make BLKSIZE and LBSIZE 512 for smaller machines */
+#define	BLKSIZE	4096
+#define	NBLK	2047
+#define	FNSIZE	128
+#define	LBSIZE	4096
+#define	ESIZE	256
+#define	GBSIZE	256
+#define	NBRA	5
+#define	KSIZE	9
+#define	CBRA	1
+#define	CCHR	2
+#define	CDOT	4
+#define	CCL	6
+#define	NCCL	8
+#define	CDOL	10
+#define	CEOF	11
+#define	CKET	12
+#define	CBACK	14
+#define	CCIRC	15
+#define	STAR	01
+#define	READ	0
+#define	WRITE	1
+
+char *getblock(unsigned int atl, int iof);
+char *_getline(unsigned int tl);
+int advance(char *lp, char *ep);
+int append(int (*f)(void), unsigned int *a);
+int backref(int i, char *lp);
+void blkio(int b, char *buf, ssize_t (*iofcn)(int, const void*, size_t)); //int(*) (int, char*, int)
+void rblkio(int b, char *buf, ssize_t (*iofcn)(int, void*, size_t));
+int cclass(char *set, int c, int af);
+void commands(void);
+void compile(int eof);
+void error(char *s);
+int execute(unsigned int *addr);
+void exfile(void);
+void filename(int comm);
+int getchr(void);
+int getfile(void);
+int getnum(void);
+void global(int k);
+void init(void);
+unsigned int *address(void);
+void newline(void);
+void nonzero(void);
+void onhup(int n);
+void onintr(int n);
+void print(void);
+void putchr(int ac);
+void putd(void);
+void putfile(void);
+int putline(void);
+void quit(int n);
+void setwide(void);
+void setnoaddr(void);
+void squeeze(int i);
+void grepline(void); char grepbuf[GBSIZE]; void greperror(char);
+
